9afe6fbfa99f8c6467817998e3657ae5d1af9efb
SPC <SPC@20-LAB402>
2024-08-12 19:51:00 +0700

    Add project files.

---
+++ Childform1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp_parent
+{
+    public partial class Childform1 : Form
+    {
+        public Childform1()
+        {
+            InitializeComponent();
+        }
+
+        private void Childform1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp_parent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void childFormToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Childform1 cf1= new Childform1();
+            cf1.MdiParent = this;
+            cf1.Show();
+        }
+    }
+}
